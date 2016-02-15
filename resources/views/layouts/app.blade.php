40867cf8b9f3b5f4ca8d62a17ea14fedef074ff1
Akash Kulshrestha <akash.kulshreshtha@enukesoftware.com>
2016-02-15 14:37:44 +0530

    Update akash

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,12 +76,21 @@
 		</header>
 		
 		@yield('content')
-
 		<script type="text/javascript" src="/js/jquery-1.11.3.min.js"></script>	
 		<script type="text/javascript" src="/js/bootstrap.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		
 		{{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
- 
+		<script type="text/javascript" >
+				$('#checkboxG2').click(function(e){
+					//var pass=$("#showpassword").val();
+					//$("#showpassword").val("");
+					$("#showpassword").focus();
+					//$("#showpassword").val(pass);
+					
+				});
+			
+		</script>
 	</body>
 </html>
