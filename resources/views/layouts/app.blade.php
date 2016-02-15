ce5d2a974f2fcaaec534643e53c3124c17e83150
Akash Kulshrestha <akash.kulshreshtha@enukesoftware.com>
2016-02-15 16:07:49 +0530

    Update akash

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -80,17 +80,15 @@
 		<script type="text/javascript" src="/js/bootstrap.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-		
 		{{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
 		<script type="text/javascript" >
-				$('#checkboxG2').click(function(e){
-					//var pass=$("#showpassword").val();
-					//$("#showpassword").val("");
-					$("#showpassword").focus();
-					//$("#showpassword").val(pass);
-					
-				});
-			
+			$('.password-eye').change(function(){
+				 if($('.password-eye').is(':checked')) 
+				 	  $('#showpassword').prop('type', 'text');
+				 else
+				 	  $('#showpassword').prop('type', 'password');
+				 $("#showpassword").focus();
+			});
 		</script>
 	</body>
 </html>
