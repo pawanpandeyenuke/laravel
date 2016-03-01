b97b2b9d3f1a9f5f7ccb49b8f5a9d4683d364149
root <root@ip-172-31-25-116.us-west-2.compute.internal>
2016-03-01 11:07:04 +0000

    possible conflict

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -161,7 +161,8 @@
 												@foreach($data->comments as $commentsData)
 												<?php 
 													$username = DB::table('users')->where('id', $commentsData->commented_by)->get(['first_name', 'last_name']);
-
+													
+													if(!empty($username)){
 													$name = $username[0]->first_name.' '.$username[0]->last_name; 
 
 												if($counter < 4){ ?>
@@ -170,7 +171,7 @@
 														<a href="<?php echo 'profile/'.$commentsData->commented_by ?>" title="" class="user-link">{{$name}}</a>
 														<div class="comment-text">{{$commentsData->comments}}</div>
 													</li>
-												<?php }$counter++; ?>
+												<?php }$counter++; } ?>
 												@endforeach
 											</ul>
 										</div><!--/comments list-->
