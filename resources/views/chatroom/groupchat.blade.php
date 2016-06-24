80218577c6b40a9f9eeaa9cffa911ca911117384
enuke <enuke@d-18.(none)>
2016-06-24 18:08:20 +0530

    update

--- resources/views/chatroom/groupchat.blade.php
+++ resources/views/chatroom/groupchat.blade.php
@@ -544,9 +544,7 @@ function hideOpendBox( grpname , actiontype ){
 }
 
 function openChatGroup( grpjid,grpname ){
-	if( hideOpendBox( grpjid+conferencechatserver , 1 ) ){
-		conObj.rooms.open( grpjid+conferencechatserver , grpname );
-	}
+	conObj.rooms.open( grpjid+conferencechatserver , grpname );
 }
 
 $('.status-r-btn').on('click',function(){
