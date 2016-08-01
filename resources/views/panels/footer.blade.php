81124cc082e99a5f1a8ac89ea45a3ab93785b994
enuke <enuke@sanjubhai.(none)>
2016-08-01 17:05:59 +0530

    Changes

---
+++ resources/views/panels/footer.blade.php
@@ -0,0 +1,8 @@
+<footer>
+ 	<div class="container">
+        <ul class="f-links text-center list-inline">
+            <li>&copy; 2016 FriendzSquare</li>       
+            <li><a href="{{ url('terms-conditions') }}" title="">Terms of Use</a></li>
+        </ul>
+    </div>
+</footer>
