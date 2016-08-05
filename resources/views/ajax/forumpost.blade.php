a0681c3ca240d449985618d0b2032f50f6e022f8
pawan pandey <pawan.pandey@enukesoftware.com>
2016-08-05 15:25:36 +0530

    msg

--- resources/views/ajax/forumpost.blade.php
+++ resources/views/ajax/forumpost.blade.php
@@ -5,18 +5,24 @@
 		</a>
 		<span class='p-date'><i class='flaticon-days'></i> {{$forumpostid->updated_at->format('d M Y')}}</span>
 		<span class='p-time'><i class='flaticon-time'></i> {{$forumpostid->updated_at->format('h:i A').' (UTC)'}}</span>
-		
-		<div class="p-likes">
+	</div>
+	<div class='f-post-title'>
+	<a href="{{url("profile/$user->id")}}" title='User Profile'>
+		{{$name}} </a>
+
+		<?php $rightClass = 'right'; ?>
+		@if(Auth::User()->id == $user->id)
+			<?php $rightClass = ''; ?>
+		@endif
+
+		<div class="p-likes custm_p_likes <?= $rightClass ?>">
 			<div class="like-cont">
 				<input type="checkbox" name="" id="checkbox_forumpost_{{$forumpostid->id}}" class="css-checkbox likeforumpost" data-forumpostid="{{$forumpostid->id}}" />
 				<label for="checkbox_forumpost_{{$forumpostid->id}}" title="Like Post" class="css-label"></label>
 			</div>
 			<span class="plike-count" title="Likes">0</span>
 		</div>
-	</div>
-	<div class='f-post-title'>
-	<a href="{{url("profile/$user->id")}}" title='User Profile'>
-		{{$name}} </a>
+
 		<div class='fp-action'>
 			<button class='editforumpost' value='{{$forumpostid->id}}'title="Edit Post"><i class='flaticon-pencil' data-breadcrum = "{{$breadcrum}}"></i></button>
 			<button class='del-confirm-forum' value='{{$forumpostid->id}}' data-forumtype = "post" data-breadcrum = "{{$breadcrum}}" title="Delete Post"><i class='flaticon-garbage'></i></button>
