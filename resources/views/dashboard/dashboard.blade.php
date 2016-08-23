7073b3eb7f44233e5b60c92b0585cc8defd09c20
enuke <enuke@sanjubhai.(none)>
2016-08-23 11:09:02 +0530

    unable to post newsfeed throuh mobile website

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -305,36 +305,12 @@
 
 <script type="text/javascript">
 jQuery(function($){
-	$(document).on("click",".btn-postt",function(){
-		// Opera 8.0+
-		// var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
-		//     // Firefox 1.0+
-		// var isFirefox = typeof InstallTrigger !== 'undefined';
-		//     // At least Safari 3+: "[object HTMLElementConstructor]"
-		// var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
-		//     // Internet Explorer 6-11
-		// var isIE = /*@cc_on!@*/false || !!document.documentMode;
-		//     // Edge 20+
-		// var isEdge = !isIE && !!window.StyleMedia;
-		//     // Blink engine detection
-		// var isBlink = (isChrome || isOpera) && !!window.CSS;
-		
-		// Chrome 1+
-		var isChrome = !!window.chrome && !!window.chrome.webstore;
-		if(isChrome!="") {
-			$(this).parents('form').submit();
-		}
-		
-		if($('#newsfeed').val()!="" || !($('#image-holder').is(':empty')))
-		$('.btn-post').prop('disabled',true);
-	});
-
 	$(document).on("click","#status_up_btn",function(){
 		$('#image-holder').empty();
 		$('#fileUpload').val('');
 		$('.badge').html('');
 	});
-
+	
 	// Post status updates via ajax call.
 	$("#postform").ajaxForm({
 		beforeSubmit: function(){
