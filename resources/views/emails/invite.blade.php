4b4894746f1d16621498ace9170e73b324bf2bd4
pawan pandey <pawan.pandey@enukesoftware.com>
2016-08-23 11:31:05 +0530

    msg

--- resources/views/emails/invite.blade.php
+++ resources/views/emails/invite.blade.php
@@ -1,219 +1,55 @@
-    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-    <html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-      <title>[SUBJECT]</title>
-      <style type="text/css">
-      body {
-       padding-top: 0 !important;
-       padding-bottom: 0 !important;
-       padding-top: 0 !important;
-       padding-bottom: 0 !important;
-       margin:0 !important;
-       width: 100% !important;
-       -webkit-text-size-adjust: 100% !important;
-       -ms-text-size-adjust: 100% !important;
-       -webkit-font-smoothing: antialiased !important;
-     }
-     .tableContent img {
-       border: 0 !important;
-       display: block !important;
-       outline: none !important;
-     }
-     a{
-      color:#382F2E;
-    }
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>FS Mailer</title>
 
-    p, h1{
-      color:#382F2E;
-      margin:0;
-    }
- p{
-      text-align:left;
-      color:#999999;
-      font-size:14px;
-      font-weight:normal;
-      line-height:19px;
-    }
-
-    a.link1{
-      color:#382F2E;
-    }
-    a.link2{
-      font-size:16px;
-      text-decoration:none;
-      color:#ffffff;
-    }
-
-    h2{
-      text-align:left;
-       color:#222222; 
-       font-size:19px;
-      font-weight:normal;
-    }
-    div,p,ul,h1{
-      margin:0;
-    }
-
-    .bgBody{
-      background: #ffffff;
-    }
-    .bgItem{
-      background: #ffffff;
-    }
-
-    </style>
-<script type="colorScheme" class="swatch active">
-{
-    "name":"Default",
-    "bgBody":"ffffff",
-    "link":"382F2E",
-    "color":"999999",
-    "bgItem":"ffffff",
-    "title":"222222"
-}
-</script>
+   
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
   </head>
-  <body paddingwidth="0" paddingheight="0"   style="padding-top: 0; padding-bottom: 0; padding-top: 0; padding-bottom: 0; background-repeat: repeat; width: 100% !important; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; -webkit-font-smoothing: antialiased;" offset="0" toppadding="0" leftpadding="0">
-    <table width="100%" border="0" cellspacing="0" cellpadding="0" class="tableContent bgBody" align="center"  style='font-family:Helvetica, Arial,serif;'>
+  <body>
+		<div style="width:680px;margin:0 auto;">
+			<div style="background-color:#EDEDED;height:10px;width:680px;margin-bottom:3px;"></div>
+			
+			<div style="background-color:#A6FFED;padding:0 15px;height:78px;">
+			<img src="{{url('/images/logo.jpg')}}" alt="" title=""/>
+			</div>
+			
+			<div style="background-color:#EDEDED;height:10px;width:680px; margin-bottom:20px;"></div>
+			
+			<div style="background-color:#EDEDED;height:380px;padding:35px 50px 0 20px;">
+				
+				<div style="background-color:#fff;padding:10px 0px 20px 15px;margin-bottom:25px;">
+				<p style="font-size:18px;">Hi,</p>
+				
+				<p style="font-size:18px;">{{ $username }} has Joined Friendszsquare. On Friendzsquare you can make new friends by joining <strong>Chat Rooms</strong>, Participate in <strong>Forums</strong>, post comments on <strong>Newsfeed</strong> and much more. </p>
+				
+				<p style="font-size:18px;margin-bottom:30px;">Explore and grow your social Network.</p>
+				
+				<a href="{{url('profile/'.$id)}}" style="background-color:#A6FFED;color:#000;padding:10px 20px;margin-bottom:20px;text-decoration:none;">Accept Invite<a/>
+				
+				</div>
+				
+				<p style="font-size:18px;margin:0;">This email has been sent to <a href="#">{{ $userobj->email }}</a></p>
+				<p style="font-size:18px;margin:0;">It is an invitation to connect. <a href="#">Unsubscribe</a></p>
+				<p style="font-size:18px;margin-top:0;margin-bottom:15px;">If you have any questions, then please contact at <a href="#">contact@friendzsquare.com </a>	
+				</p>
 
-      
-      <tr><td height='35'></td></tr>
-
-      <tr>
-        <td>
-          <table width="600" border="0" cellspacing="0" cellpadding="0" align="center" class='bgItem'>
-            <tr>
-              <td width='40'></td>
-              <td width='520'>
-                <table width="520" border="0" cellspacing="0" cellpadding="0" align="center">
-
-                  <tr><td height='75' style="padding-bottom: 10px; border-bottom: 3px solid #A0F0E8;"><a href="#" title=""><img src="{{url('/images/logo.jpg')}}" alt=""></a></td></tr>
-                  <tr>
-                    <td class='movableContentContainer' valign='top' style="padding-top: 20px;">
-
-                      <div lass='movableContent'>
-                        <table width="520" border="0" cellspacing="0" cellpadding="0" align="center">
-                          <tr>
-                            <td valign='top' align='center'>
-                              <div class="contentEditableContainer contentTextEditable">
-                                <div class="contentEditable">
-
-                                  <p style='text-align:center;margin:0;font-family:Georgia,Time,sans-serif;font-size:26px;color:#222222;'>Welcome to <a href="{{url('/')}}" style="text-decoration:none"><span style='color:#A0F0E6;'>Friendz Square</span></a></p>
-
-                                </div>
-                              </div>
-                            </td>
-                          </tr>
-                        </table>
-                      </div>
-
-                      <div lass='movableContent'>
-                        <table width="520" border="0" cellspacing="0" cellpadding="0" align="center">
-                          <tr>
-                            <td valign='top' align='center'>
-                              <div class="contentEditableContainer contentImageEditable">
-                              </div>
-                            </td>
-                          </tr>
-                        </table>
-                      </div>
-
-                      <div class='movableContent'>
-                        <table width="520" border="0" cellspacing="0" cellpadding="0" align="center">
-                          <tr><td height='30'></td></tr>
-                          <tr>
-                            <td align='left'>
-                              <div class="contentEditableContainer contentTextEditable">
-                                <div class="contentEditable" align='center'>
-
-                                  <h2> Connect to your friends and stay updated about their lives!</h2>
-
-                                </div>
-                              </div>
-                            </td>
-                          </tr>
-
-                          <tr><td height='15'> </td></tr>
-
-                          <tr>
-                            <td align='left'>
-                              <div class="contentEditableContainer contentTextEditable">
-                                <div class="contentEditable" align='center'>
-                                  <p  style='text-align:left;color:#999999;font-size:14px;font-weight:normal;line-height:19px;'>
-                                    Hi, <br/> <br/> {{ $username }} has joined Friendzsquare. On FriendzSqaure you can make new friends by joining chat rooms, participate in forums, post comments on newsfeed and much more. </br> </br> Explore and grow your social network.<!--  <a target='_blank' class='link1' href="{{url('profile/'.$id)}}"> clicking here</a> -->
-
-                                    <!-- <span style='color:#222222;'>{{$username}}</span> -->
-
-                                  </p>
-                                </div>
-                              </div>
-                            </td>
-                          </tr>
-
-                          <tr><td height='55'></td></tr>
-
-                          <tr>
-                            <td align='center'>
-                              <table>
-                                <tr>
-                                  <td align='center' bgcolor='#A0F0E6' style='background:#A0F0E6; padding:15px 18px;-webkit-border-radius: 4px; -moz-border-radius: 4px; border-radius: 4px;'>
-                                    <div class="contentEditableContainer contentTextEditable">
-                                      <div class="contentEditable" align='center'>
-
-                                        <a target='_blank' href="{{url('profile/'.$id)}}" class='link2' style='color:#ffffff;'>Accept Invite</a>
-
-                                      </div>
-                                    </div>
-                                  </td>
-                                </tr>
-                              </table>
-                            </td>
-                          </tr>
-                          <tr><td height='20'></td></tr>
-                        </table>
-                      </div>
-
-                      <div lass='movableContent'>
-                        <table width="520" border="0" cellspacing="0" cellpadding="0" align="center">
-                          <tr><td height='35'></td></tr>
-                          <tr><td  style='border-bottom:1px solid #DDDDDD;'></td></tr>
-
-                          <tr><td height='25'></td></tr>
-
-                          <tr>
-                            <td>
-                              <table width="520" border="0" cellspacing="0" cellpadding="0" align="center">
-                                <tr>
-                                  <td valign='top' align='left' width='370'>
-                                    <div class="contentEditableContainer contentTextEditable">
-                                      <div class="contentEditable" align='center'>
-                                        <p  style='text-align:left;color:#CCCCCC;font-size:12px;font-weight:normal;line-height:20px;'>                                        
-                                          <a target='_blank' style="margin-left:40px;" href="#">Forward to a friend</a>
-                                          <a target='_blank' style="margin-left:50px;" href="#" class='link1' class='color:#382F2E;'>Unsubscribe</a>
-                                          <a target='_blank' style="margin-left:50px;" class='link1' class='color:#382F2E;' href="#">Show this email in your browser</a>
-                                        </p>
-                                      </div>
-                                    </div>
-                                  </td>
-                                </tr>
-                              </table>
-                            </td>
-                          </tr>
-                        </table>
-                      </div>
-
-                    </td>
-                  </tr>
-                </table>
-              </td>
-              <td width='40'></td>
-            </tr>
-          </table>
-        </td>
-      </tr>
-      <tr><td height='88'></td></tr>
-    </table>
-      </body>
-      </html>
- 
+				<a href="#" style="text-decoration:none;font-size:18px;">You may see this email in browser</a>
+				<a href="#" style="float:right;text-decoration:none;">Forward to you friend</a>
+				
+			</div>
+			
+			
+		</div>
+   
+  </body>
+</html>
