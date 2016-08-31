2923f5f6fce01998828abbf653a0718bd29e7115
enuke <enuke@sanjubhai.(none)>
2016-08-31 11:58:54 +0530

    Changes

--- resources/views/privacy-policy.blade.php
+++ resources/views/privacy-policy.blade.php
@@ -93,8 +93,8 @@
 	<footer>
 	 	<div class="container">
 	        <ul class="f-links text-center list-inline">
-	            <li>&copy; 2016 FriendzSquare</li>       
-	            <li><a href="{{ url('terms') }}" title="Terms">Terms of Use</a></li>
+	            <li>Copyright 2016 Connect All Pte Ltd.</li>
+	            <li><a href="{{ url('terms') }}" title="Terms of Use">Terms of Use</a></li>
 	            <li><a href="{{ url('privacy-policy') }}" title="Privacy Policy">Privacy Policy</a></li>
 	        </ul>
 	    </div>
