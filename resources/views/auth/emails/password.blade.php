9a3287b4ac31bc77d16b4241cd40c4cb43753224
pawan pandey <pawan.pandey@enukesoftware.com>
2016-09-05 17:56:20 +0530

    msg

--- resources/views/auth/emails/password.blade.php
+++ resources/views/auth/emails/password.blade.php
@@ -23,18 +23,18 @@
       <div style="background-color:#5df7e3;height:8px; margin-bottom:12px;"></div>
       <div style="background-color:#fff;padding:10px 15px;margin-bottom:10px;">
         <p style="font-size:16px;font-weight:600;">Hi {{ $user->first_name.' '.$user->last_name }},</p>
-        <p style="font-size:14px;line-height:32px;">We recently received a password change request from you. To change your FriendzSquare password click below:</p>
+        <p style="font-size:14px;line-height:20px;">We recently received a password change request from you. To change your FriendzSquare password click below:</p>
         
         <p style='margin-top:20px;font-size:14px;margin-bottom:25px;text-align:center;'>
         <a href="{{ $link = url('password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"   target="_blank" style="background: #5df7e3;color: #000;height:32px;padding:10px 12px;text-decoration: none;border-radius: 3px;">Change Password</a></p>
         
         <h4 style="text-align:center;margin:10px 0 8px;font-size:20px;">OR</h4>
         
-        <p style="font-size:14px;line-height:32px;">Or you can paste the following link into your browser: <a href="{{ $link }}" target="_blank"> {{ $link }} </a></p>
+        <p style="font-size:14px;line-height:20px;">You can paste the following link into your browser: <a href="{{ $link }}" target="_blank"> {{ $link }} </a></p>
         
         <p style='font-size:14px;'> The link will expire in 24 hours.</p>
         
-        <p style='margin:0;font-size:14px;color:#0d0d0d;line-height:32px;'> If you did not request the password change, please immediately email to <a href="mailto:contact@friendzsquare.com" style="font-weight:700">contact@friendzsquare.com</a> </p>
+        <p style='margin:0;font-size:14px;color:#0d0d0d;line-height:20px;'> If you did not request the password change, please immediately email to <a href="mailto:contact@friendzsquare.com" style="font-weight:700">contact@friendzsquare.com</a> </p>
       
         <p style='font-size:14px;color:#0d0d0d;'>To keep your account secure, please don’t forward this email or link to anyone. </p>
         
