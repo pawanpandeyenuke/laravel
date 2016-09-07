22be0072f9ddb027fc0c1c0d6adc890f48b836f8
pawan pandey <pawan.pandey@enukesoftware.com>
2016-09-07 16:36:17 +0530

    msg

--- resources/views/panels/email-template.blade.php
+++ resources/views/panels/email-template.blade.php
@@ -15,30 +15,41 @@
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
   </head>
-  <body style="font-family: 'Open Sans', sans-serif;background-color:#e9faf8;width:100%;float:left;">
-  
-    <div style="width:590px;margin:30px auto;">
-      <div style="background-color:#fff;padding:10px 15px;">
-      <a href="#"><img src="{{url('/images/logo.jpg')}}" alt="" title=""/></a>
-      </div>
-      <div style="background-color:#5df7e3;height:8px; margin-bottom:12px;"></div>
-      <div style="background-color:#fff;padding:10px 15px;margin-bottom:10px;">
-        <p style="font-size:16px;font-weight:600;">Hi <?= ucwords($user_name) ?>,</p><br>
-        <p style='text-align:left;color:#0d0d0d;font-size:14px;font-weight:normal;line-height:19px;'>
-          {{ $current_data }}
-        </p>
-        <p style='text-align:left;color:#0d0d0d;font-size:14px;font-weight:normal;line-height:19px;'>
-          {{ $type }} {{ $post_message }}
-        </p>
-        <br>
-        <p style="text-align:left;color:#0d0d0d;font-size:14px;font-weight:normal;line-height:19px;">
-          <a title="" href="{{ $post_url }}" style="text-decoration: none;-moz-user-select: none; background-image: none; border: 1px solid transparent; border-radius: 4px; cursor: pointer; display: inline-block; font-size: 14px; font-weight: normal; line-height: 1.42857; margin-bottom: 0; padding: 6px 12px; text-align: center; vertical-align: middle; white-space: nowrap; background: #a0f0e6 none repeat scroll 0 0; color: #000; font-size: 15px;">Go To {{ $linktype }}</a>
+  <body style="font-family: 'Open Sans', sans-serif;background-color:#e9faf8;">
+    <div style="background-color:#e9faf8;width:100%;float:left;">
+      <div style="width:590px;margin:30px auto;">
+        <div style="background-color:#fff;padding:10px 15px;">
+        <a href="#"><img src="{{url('/images/logo.jpg')}}" alt="" title=""/></a>
+        </div>
+        <div style="background-color:#5df7e3;height:8px; margin-bottom:12px;"></div>
+        <div style="background-color:#fff;padding:10px 15px;margin-bottom:10px;">
+          <p style="font-size:16px;font-weight:600;">Hi <?= ucwords($user_name) ?>,</p><br>
+          <p style='text-align:left;color:#0d0d0d;font-size:14px;font-weight:normal;line-height:19px;'>
+            {{ $current_data }}
+          </p>
+          <p style='text-align:left;color:#0d0d0d;font-size:14px;font-weight:normal;line-height:19px;'>
+            {{ $type }} {{ $post_message }}
+          </p>
           <br>
-        </p>
+          <p style="text-align:left;color:#0d0d0d;font-size:14px;font-weight:normal;line-height:19px;">
+            <a title="" href="{{ $post_url }}" style="text-decoration: none;-moz-user-select: none; background-image: none; border: 1px solid transparent; border-radius: 4px; cursor: pointer; display: inline-block; font-size: 14px; font-weight: normal; line-height: 1.42857; margin-bottom: 0; padding: 6px 12px; text-align: center; vertical-align: middle; white-space: nowrap; background: #a0f0e6 none repeat scroll 0 0; color: #000; font-size: 15px;">Go To {{ $linktype }}</a>
+            <br>
+          </p>
 
+        </div>
+        <div style="background-color:#e9faf8;padding:10px 20px;text-align:center;margin-bottom:20px;">
+        
+          <p style="font-size:16px;margin-top:20px;margin-bottom:0;">This email has been sent to <a href="#" style="color:#000;font-weight:bold;">{{ $user_email }}</a></p>
+
+          <p style="font-weight:bold;margin-bottom:10px;">Don’t want forum notification?</p>
+          <a href="{{ url('forums/unsubscribe?token='.$access_token) }}" target='_blank' style="background-color:#5df7e3;color:#000;padding:10px 20px;margin-bottom:25px;text-decoration:none;text-transform:capitalize;font-weight:bold;">Unsubscribe</a>
+        </div>
+
+        <div style="background-color:#fff;padding:10px 15px;text-align:center;">
+        <p style="font-size:12px;margin-top:15px;margin-bottom:15px;">If you have any questions, then please contact at <a href="mailto:contact@friendzsquare.com" style="color:#000;font-weight:bold;">contact@friendzsquare.com </a></p>
+        </div>
+        <div style="background-color:#5df7e3;height:8px; margin-bottom:15px;font-size:0px;">Copyright</div>  
       </div>
-      
-      <div style="background-color:#5df7e3;height:8px; margin-bottom:15px;font-size:0px;">Copyright</div>  
     </div>
   </body>
 </html>
