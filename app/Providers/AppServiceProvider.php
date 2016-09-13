32968c5158136f581060a0efdd2efa646a22a1b5
pawan pandey <pawan.pandey@enukesoftware.com>
2016-09-13 10:01:32 +0530

    msg

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,18 +16,32 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        
-/*        $forum = Forums::where('parent_id', 0)->pluck('id')->toArray();
-        foreach ($forum as $value) {
-            $forumdata = Forums::find($value);
-            $forumdata->img_url = $value.'.png';
-            $forumdata->save();
-        }
-        echo '<pre>';print_r($forumdata);die; */
 
-        $educationLevel = array('High school','Certificate/diploma','Associate degree','3 or 4 year undergraduate program','Post graduate degree','Post graduate degree - MBA','Post graduate degree - Masters','Post graduate degree - JD','Post graduate degree - PHD/Doctrate','Professional','Other degree');
+        $educationLevel = array(
+                'High school',
+                'certifciate/diploma',
+                'Associate degre',
+                '3 or 4 year undergraduate program',
+                'Post graduate degree',
+                'Post graduate degree - MBA',
+                'Post graduate degree - Masters',
+                'Post graduate degree - JD',
+                'Post graduate degree - PHD/Doctrate',
+                'Medical Doctor (M.D.)',
+                'Doctor of Dental Surgery (D.D.S.)',
+                'Doctor of Pharmacy (Pharm.D.)',
+                'CA',
+                'CFA',
+                'ACCA',
+                'CPA',
+                'FRM',
+                'IAS',
+                'Other professional degree',
+                'Others'
+                );
 
-        $specialization = array('Accounting','Arts','Economics','Engineer','English','Finance','HR','IT','Marketing','Mathematics','Medicine','Operations','Others');
+
+        $specialization = array('Accounting ','Agriculture & Forestry','Anthropology','Archaeology','Architecture','Art & Design','Biological Sciences','Business & Management','Chemistry','CiviI & Structural Engineering','Communication & Media Studies','Computer Science','Dentistry','Development Studies','Earth & Marine Sciences','Economics & Econometrics','Education','Engineering - Aeronautical','Engineering - Chemical','Engineering – Civil and Structural','Engineering – Computer science','Engineering – Electrical & Electronic','Engineering – Manufacturing','Engineering – Mechanical','Engineering – Mining and Mineral','Engineering –Graphics','English Language & Literature','Environmental Sciences','Film Studies','Finance','Geography','Health and Nutrition','History','Law','Life Sciences & Medicine','Linguistics','Materials Sciences','Mathematics','Medicine','Modern Languages','Music','Natural Sciences','Nursing','Operational research','Performing Arts','Pharmacy & Pharmacology','Philosophy','Physics & Astronomy','Politics and International Studies','Psychology','Social Policy and Administration','Social Sciences','Sociology','Statistics','Veterinary Science','Others');
 
         $gradYear = array("1990","1991","1992","1993","1994","1995","1996","1997","1998","1999","2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012","2013","2014","2015");
 
